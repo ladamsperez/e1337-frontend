a06d9d4ae44f2000713ce198ae605c04f95e1a27
ladamsperez <ladamsperezwork@gmail.com>
2021-10-04 08:28:55 -0400

    added info page

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
 import "./App.css";
 import styled from "styled-components";
-// import { AccountBox } from "./components/accountBox";
+import { AccountBox } from "./components/accountBox";
 import About from './components/About';
+import Info from './components/Info';
 import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 function App() {
   return (
     <Router>
-      <Route path="/" component={Home}/>
-      <Route path="/about" component={About}/>
-      {/* <AppContainer>
-        <AccountBox />
-      </AppContainer> */}
+      <Switch>
+        <Route path="/" exact component={Home}/>
+        <Route path="/about" component={About}/>
+        <Route path="/info" component={Info}/>
+        <Route path="/login" component={AccountBox}/>
+      </Switch>
     </Router>
   );
 }
 
-// const AppContainer = styled.div`
-//   width: 100%;
-//   height: 100%;
-//   display: flex;
-//   flex-direction: column;
-//   align-items: center;
-//   justify-content: center;
-// `;
+const AppContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
 
 
 const Home = () => (
